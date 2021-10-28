fbd22e72b949ff39023f2aec5c4afa6ca4254ff3
bityob <bityob@gmail.com>
2021-10-28 04:39:59 +0300

    fix bash script

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+export DOCKER_SCAN_SUGGEST=false
+
 docker build . -t podcasts-list-to-rss --build-arg TELEGRAM_APP_ID=${TELEGRAM_APP_ID} --build-arg TELEGRAM_APP_HASH=${TELEGRAM_APP_HASH} --build-arg TELEGRAM_PUBLIC_CHANNEL_NAME=${TELEGRAM_PUBLIC_CHANNEL_NAME}
 
 docker run podcasts-list-to-rss
