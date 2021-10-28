8cfa3546c7113f31e5b02b3b3502645108509ad0
bityob <bityob@gmail.com>
2021-10-28 04:24:50 +0300

    Update RSS order (newere on the top)

--- src/rss.py
+++ src/rss.py
@@ -40,7 +40,7 @@ class RssGenerator:
         # Rss with only basic fields
         rss_string = str(self.p)
 
-        for message in reversed(self.messages):
+        for message in self.messages:
             try:
                 print(f"Message id={message.id}...")
                 connector = PocketCasts(message.url)
