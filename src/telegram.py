9956e8dfb5377d03090b9818c724c3859f0285db
bityob <bityob@gmail.com>
2021-10-28 05:00:31 +0300

    Add phone and passwords to secrets

--- src/telegram.py
+++ src/telegram.py
@@ -9,6 +9,8 @@ from base import Message
 api_id = os.environ["TELEGRAM_APP_ID"]
 api_hash = os.environ["TELEGRAM_APP_HASH"]
 channel_name = os.environ["TELEGRAM_PUBLIC_CHANNEL_NAME"]
+phone = os.environ["TELEGRAM_PHONE"]
+password = os.environ["TELEGRAM_PASSWORD"]
 
 
 class TelegramReader:
@@ -20,7 +22,10 @@ class TelegramReader:
 
     async def gen_messages(self) -> Iterable[Message]:
         async with TelegramClient('user', api_id, api_hash) as client:
-            await client.start()
+            await client.start(
+                phone=phone,
+                password=password,
+            )
 
             async for message in client.iter_messages(channel_name, limit=100):
                 print(f"id={message.id}, date={message.date}")
