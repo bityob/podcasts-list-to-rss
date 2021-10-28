a553628a40a957da2afda1470df3def62683a211
bityob <bityob@gmail.com>
2021-10-28 05:15:17 +0300

    Clearing phone and password usages

--- src/telegram.py
+++ src/telegram.py
@@ -9,8 +9,6 @@ from base import Message
 api_id = os.environ["TELEGRAM_APP_ID"]
 api_hash = os.environ["TELEGRAM_APP_HASH"]
 channel_name = os.environ["TELEGRAM_PUBLIC_CHANNEL_NAME"]
-phone = os.environ["TELEGRAM_PHONE"]
-password = os.environ["TELEGRAM_PASSWORD"]
 
 
 class TelegramReader:
@@ -22,12 +20,9 @@ class TelegramReader:
 
     async def gen_messages(self) -> Iterable[Message]:
         async with TelegramClient('user', api_id, api_hash) as client:
-            await client.start(
-                phone=phone,
-                password=password,
-            )
+            await client.start()
 
-            async for message in client.iter_messages(channel_name, limit=100):
+            async for message in client.iter_messages(channel_name, limit=1):
                 print(f"id={message.id}, date={message.date}")
                 if message.text:
                     yield Message(
