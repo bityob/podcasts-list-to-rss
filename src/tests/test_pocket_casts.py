df2863d50476ce01a8d7c1f79cb7d6521ccd3ce9
Yonatan Bitton <bityob@gmail.com>
2025-07-21 10:56:26 +0300

    Run precommit all

--- src/tests/test_pocket_casts.py
+++ src/tests/test_pocket_casts.py
@@ -1,16 +1,19 @@
 import pytest
 
 
-@pytest.mark.parametrize("url, expected_match", [
-    (
-        "https://pocketcasts.com/podcasts/02f40f60-527e-0134-ec21-0d50f522381b/158aeb67-e8d7-4b51-b26c-f2e9bf87a720",
-        ("02f40f60-527e-0134-ec21-0d50f522381b", "158aeb67-e8d7-4b51-b26c-f2e9bf87a720")
-    ),
-    (
+@pytest.mark.parametrize(
+    "url, expected_match",
+    [
+        (
+            "https://pocketcasts.com/podcasts/02f40f60-527e-0134-ec21-0d50f522381b/158aeb67-e8d7-4b51-b26c-f2e9bf87a720",
+            ("02f40f60-527e-0134-ec21-0d50f522381b", "158aeb67-e8d7-4b51-b26c-f2e9bf87a720"),
+        ),
+        (
             "https://play.pocketcasts.com/podcasts/02f40f60-527e-0134-ec21-0d50f522381b/158aeb67-e8d7-4b51-b26c-f2e9bf87a720",
-            ("02f40f60-527e-0134-ec21-0d50f522381b", "158aeb67-e8d7-4b51-b26c-f2e9bf87a720")
-    )
-])
+            ("02f40f60-527e-0134-ec21-0d50f522381b", "158aeb67-e8d7-4b51-b26c-f2e9bf87a720"),
+        ),
+    ],
+)
 def test_get_podcast_id_from_response_url(url, expected_match):
     from src.pocket_casts import PocketCasts
 
